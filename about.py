1edec5d6329b7d3d801365fd0cb2bad3b2575d0a
Harsh Srivastava <harshme08@gmail.com>
2025-03-04 01:24:55 +0530

    Update about.py

--- about.py
+++ about.py
@@ -6,6 +6,7 @@ st.set_page_config(page_title="Streamlit App with Navbar", layout="wide")
 # Navbar items
 def navbar():
     st.markdown(
+        
         """
         <style>
             .navbar {
