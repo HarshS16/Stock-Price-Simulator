7705b08c020dda3dc2a1d4008cf4c84aa25300ed
Harsh Srivastava <harshme08@gmail.com>
2025-03-29 00:41:20 +0530

    Update ai.py

--- ai.py
+++ ai.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 
 import numpy as np
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
